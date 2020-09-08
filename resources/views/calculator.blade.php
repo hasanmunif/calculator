53c0dacacc5cc41022f835b77e00e2095cf2febf
hasanmunif <m.hasanmunif@gmail.com>
2020-09-08 08:53:42 +0700

    menambahkan project calculator

---
+++ resources/views/calculator.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <input type="button" name="first" class="form-control">
+            </div>
+            <div class="col-md-3">
+                <input type="button" name="second" class="form-control">
+            </div>
+            <div class="col-md-3">
+                <select name="operasi" class="form-control">
+                    <option value="tambah"> +</option>
+                    <option value="kurang"> -</option>
+                    <option value="bagi"> /</option>
+                    <option value="kali"> *</option>
+                </select>
+            </div>
+            <div class="col-md-3">
+                <button type="submit" class="btn btn-info">hasil</button>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
